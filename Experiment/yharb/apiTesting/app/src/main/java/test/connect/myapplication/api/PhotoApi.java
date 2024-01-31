02cc623accf526a582d0ac97082f5486b644032c
yusef <131935248+yusef800@users.noreply.github.com>
2024-01-31 16:40:40 -0600

    App that tests api calls

---
+++ Experiment/yharb/apiTesting/app/src/main/java/test/connect/myapplication/api/PhotoApi.java
@@ -0,0 +1,10 @@
+package test.connect.myapplication.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import test.connect.myapplication.model.Post;
+
+public interface PhotoApi {
+    @GET("posts/1")
+    Call<Post> getFirstPost();
+}
