4418a110a0af4b05213cc8e4f6ea59785353bc33
yusef <131935248+yusef800@users.noreply.github.com>
2024-01-31 17:55:01 -0600

    Can type num and pull that photo from remote source

--- Experiment/yharb/apiTesting/app/src/main/java/test/connect/myapplication/MainActivity.java
+++ Experiment/yharb/apiTesting/app/src/main/java/test/connect/myapplication/MainActivity.java
@@ -6,8 +6,14 @@ import static test.connect.myapplication.api.ApiClientFactory.GetPostApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,14 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
 
         TextView apiText1 = findViewById(R.id.activity_main_testView1);
+        apiText1.setMovementMethod(new ScrollingMovementMethod());
 
 
         //Gets first photo from remote website and prints the values on display
-        
+
 //        GetPhotoApi().getFirstPhoto().enqueue(new SlimCallback<Photo>(responsePhoto -> {
 //            apiText1.setText(responsePhoto.printable());
 //        }));
 
+        Button photoButton = findViewById(R.id.activity_main_button_for_photo);
+        EditText photoNumInput = findViewById(R.id.activity_main_photoNum_input);
+
+
+
+//        GetPhotoApi().getAllPhoto().enqueue(new SlimCallback<List<Photo>>(photos ->{
+//
+//          apiText1.setText("");
+//            for(int i = 0;i < photos.size(); i++){
+//                apiText1.append(photos.get(i).printable());
+//            }
+//
+//        },"multiplePhotosApi"));
+
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetPhotoApi().getPhotoByNum(photoNumInput.getText().toString()).enqueue(new SlimCallback<Photo>(photo ->{
+                    apiText1.setText(photo.printable());
+                }));
+            }
+        });
 
     }
 }
