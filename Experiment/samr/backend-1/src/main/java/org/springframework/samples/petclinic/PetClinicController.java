d43cabed21f8a3b47e90fc9205603549205cd507
Sam Richter <samr888@iastate.edu>
2024-02-01 21:22:07 -0600

    use a hashmap for pets<->owners

--- Experiment/samr/backend-1/src/main/java/org/springframework/samples/petclinic/PetClinicController.java
+++ Experiment/samr/backend-1/src/main/java/org/springframework/samples/petclinic/PetClinicController.java
@@ -1,14 +1,9 @@
 package org.springframework.samples.petclinic;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.samples.petclinic.owner.OwnerRepository;
 import org.springframework.samples.petclinic.owner.Owner;
@@ -20,79 +15,93 @@ import org.springframework.samples.petclinic.pet.PetRepository;
 public class PetClinicController {
 	
 	@Autowired
-	OwnerRepository owners_repo;
+	protected OwnerRepository owners_repo;
 	@Autowired
-	PetRepository pets_repo;
+	protected PetRepository pets_repo;
+
+	protected HashMap<Integer, HashSet<Pet>> owners_to_pets = new HashMap<>();
 
 
-	/** Owners */
 
-	@RequestMapping(method = RequestMethod.POST, path = "/owners/new")
-	public String saveOwner(Owner owner) {
-		owners_repo.save(owner);
-		return "New Owner "+ owner.getFirstName() + " Saved";
+/** Owners */
+
+	/** Create a new owner */
+	@PostMapping(path = "/owners/create")
+	public @ResponseBody String saveOwner(Owner owner) {
+		if(owner == null) return "Failed to add owner -- invalid object!";
+		Owner n = owners_repo.save(owner);
+		return String.format("Successfully saved owner %s to id %d!", n.firstName, n.id);
 	}
-		// function just to create dummy data
-	@RequestMapping(method = RequestMethod.GET, path = "/owner/create")
-	public String createDummyData() {
-		final Integer[] NO_PETS = new Integer[]{};
-		owners_repo.save( new Owner(1, "John", "Doe", "404 Not found", "some numbers", NO_PETS) );
-		owners_repo.save( new Owner(2, "Jane", "Doe", "Its a secret", "you wish", NO_PETS) );
-		owners_repo.save( new Owner(3, "Some", "Pleb", "Right next to the Library", "515-345-41213", NO_PETS) );
-		owners_repo.save( new Owner(4, "Chad", "Champion", "Reddit memes corner", "420-420-4200", NO_PETS) );
-		return "Successfully created dummy data";
+	/** Saves pre-generated dummy owners to the repo */
+	@GetMapping(path = "/owners/generate")
+	public @ResponseBody String createDummyData() {
+		owners_repo.save( new Owner(0, "John", "Doe", "404 Not found", "some numbers") );
+		owners_repo.save( new Owner(0, "Jane", "Doe", "Its a secret", "you wish") );
+		owners_repo.save( new Owner(0, "Some", "Pleb", "Right next to the Library", "515-345-41213") );
+		owners_repo.save( new Owner(0, "Chad", "Champion", "Reddit memes corner", "420-420-4200") );
+		return "Successfully saved dummy data!";
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "/owners")
-	public List<Owner> getAllOwners() {
+	/** List all the owners */
+	@GetMapping(path = "/owners")
+	public @ResponseBody List<Owner> getAllOwners() {
 		return owners_repo.findAll();
 	}
-
-	@RequestMapping(method = RequestMethod.GET, path = "/owners/{ownerId}")
-	public Optional<Owner> findOwnerById(@PathVariable("ownerId") int id) {
+	/** Get an owner's data (by owner id) */
+	@GetMapping(path = "/owners/{ownerId}")
+	public @ResponseBody Optional<Owner> findOwnerById(@PathVariable("ownerId") Integer id) {
+		if(id == null) return Optional.empty();
 		return owners_repo.findById(id);
 	}
-
-	@RequestMapping(method = RequestMethod.GET, path = "/owners/{ownerId}/pets")
-	public List<Pet> findPetsByOwnerId(@PathVariable("ownerId") int id) {
-		Optional<Owner> owner = findOwnerById(id);
-		final ArrayList<Pet> pets = new ArrayList<>();
-		if(owner.isPresent()) {
-			final Owner o = owner.get();
-			for(Integer pet : o.pet_ids) {
-				Optional<Pet> p = pets_repo.findById(pet);
-				if(p.isPresent()) {
-					pets.add(p.get());
-				}
+	/** Get a list of all the pets that an owner owns */
+	@GetMapping(path = "/owners/{ownerId}/pets")
+	public @ResponseBody List<Pet> findPetsByOwnerId(@PathVariable("ownerId") Integer id) {
+		if(id != null) {
+			HashSet<Pet> pets = owners_to_pets.get(id);
+			if(pets != null) {
+				return new ArrayList<>(pets);
 			}
 		}
-		return pets;
+		return null;
 	}
 
 
-	/** Pets */
 
-	@RequestMapping(method = RequestMethod.POST, path = "/pets/new")
-	public String savePet(Pet pet) {
-		pets_repo.save(pet);
-		return "New pet " + pet.name + " saved to database";
+/** Pets */
+
+	/** Create a pet and attach to owner if owner id is valid */
+	@PostMapping(path = "/pets/create")
+	public @ResponseBody String savePet(@RequestBody Pet pet) {
+		if(pet == null) return "Failed to save pet -- invalid object!";
+		Pet p = pets_repo.save(pet);
+		if(p.owner_id != null) {
+			Optional<Owner> owner = owners_repo.findById(p.owner_id);
+			if(owner.isPresent() && this.owners_to_pets.get(owner.get().id).add(p)) {
+				return String.format("Successfully saved pet %s with id %d [owner: %s]", p.name, p.id, owner.get().firstName);
+			}
+		}
+		return String.format("Successfully saved pet %s with id %d [no owner assigned].", p.name, p.id);
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "pets")
-	public List<Pet> allPets() {
+	/** Get a list of all pets */
+	@GetMapping(path = "/pets")
+	public @ResponseBody List<Pet> allPets() {
 		return pets_repo.findAll();
 	}
-
-	@RequestMapping(method = RequestMethod.GET, path = "pets/{petId}")
-	public Optional<Pet> findPetById(@PathVariable("petId") int id) {
+	/** Get a pet by it's id */
+	@GetMapping( path = "pets/{petId}")
+	public @ResponseBody Optional<Pet> findPetById(@PathVariable("petId") Integer id) {
+		if(id == null) return Optional.empty();
 		return pets_repo.findById(id);
 	}
-
-	@RequestMapping(method = RequestMethod.GET, path = "pets/{petId}/owner")
-	public Optional<Owner> findOwnerByPetId(@PathVariable("petId") int id) {
-		Optional<Pet> pet = findPetById(id);
-		if(pet.isPresent()) {
-			return this.findOwnerById(pet.get().id);
+	/** Get a pet's owner */
+	@GetMapping(path = "pets/{petId}/owner")
+	public @ResponseBody Optional<Owner> findOwnerByPetId(@PathVariable("petId") Integer id) {
+		if(id != null) {
+			Optional<Pet> pet = pets_repo.findById(id);
+			if(pet.isPresent()) {
+				return this.findOwnerById(pet.get().id);
+			}
 		}
 		return Optional.empty();
 	}
