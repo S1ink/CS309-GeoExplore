2a339f4dd652648871814ea9b195b5aecedf4410
aditin@iastate.edu <glpat-bhuszei11sThbb1j_YJs>
2024-02-20 20:09:59 -0600

    Log in page done

--- Experiment/aditin/LogIn/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiment/aditin/LogIn/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,113 +16,58 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast; // for pop up messages
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner sp; // for drop down
-    boolean invalid = false;
-    String Colector="";
-    TextView txtalertName;
-    EditText UserName,UserPassword,UserContact,UserComment;
-    Button SubmitSave;
-    RadioButton Malebtn,Femalbtn;
-    CheckBox html,css,java1;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sp=findViewById(R.id.SpCountry);
-        UserName=findViewById(R.id.userName);
-        UserPassword=findViewById(R.id.userPassword);
-        UserContact=findViewById(R.id.userContact);
-        UserComment=findViewById(R.id.usercomment);
-        txtalertName=findViewById(R.id.userAlert);
-        Malebtn =findViewById(R.id.Male);
-        Femalbtn=findViewById(R.id.Female);
-        html=findViewById(R.id.HTML);
-        css=findViewById(R.id.CSS);
-        java1=findViewById(R.id.jav);
-        SubmitSave=findViewById(R.id.btnSubmit);
-        SubmitSave.setOnClickListener(new View.OnClickListener() {
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                String name = UserName.getText().toString();
-                String Passcode=UserPassword.getText().toString();
-                String contact=UserContact.getText().toString();
-                String comment=UserComment.getText().toString();
-                if (name.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Pleas fill the password field",Toast.LENGTH_SHORT).show();
-                }
-                else if (name.equals("Aditi") ||name.equals("Nachnani")){
-                    invalid=true;
-                    txtalertName.setText("Name Already exist");
-                }
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
 
-                else if(Passcode.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Pleas fill the password field",Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
 
+            }
 
-                else if (contact.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Pleas fill the Contact field",Toast.LENGTH_SHORT).show();
-                }
-
-                else if (comment.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Pleas fill the Comment field",Toast.LENGTH_SHORT).show();
-                }
-
-
-
-                else{
-
-                    Colector+=name+"\n";
-                    Colector+=Passcode+"\n";
-                    Colector+=contact+"\n";
-                    Colector+=comment+"\n";
-                    if (html.isChecked()){
-                        Colector+="HTML"+"\n";
-                        if (css.isChecked()){
-                            Colector+="CSS"+"\n";
-                        }
-                        if (java1.isChecked()){
-                            Colector+="Java"+"\n";
-                        }
-                    }
-                    Toast.makeText(MainActivity.this,"User Info \n:"+Colector,Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
 
-        List<String> categoryCountry=new ArrayList<>();
-        categoryCountry.add("Select Country");
-        categoryCountry.add("United States");
-        categoryCountry.add("Canada");
-        categoryCountry.add("UAE");
-        categoryCountry.add("India");
-        categoryCountry.add("Germany");
-        ArrayAdapter<String> arrayAdapter;
-        arrayAdapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,categoryCountry);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp.setAdapter(arrayAdapter);
-        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
-                if(!parent.getItemAtPosition(position).equals("Select Country")){
-                    //Do Nothing
-                    String item=parent.getItemAtPosition(position).toString();
-                    Colector+=item+"\n";
-                    Toast.makeText(MainActivity.this, "Selected Country: "+item, Toast.LENGTH_SHORT).show();
-
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
             }
         });
+
+
+
     }
 }
+
