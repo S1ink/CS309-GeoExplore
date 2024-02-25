1bfb6b12c3d43d59552eaaef1c1fd136868718d5
yusef <131935248+yusef800@users.noreply.github.com>
2024-02-25 14:32:46 -0600

    Post function almost done

--- Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/MapsActivity.java
+++ Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/MapsActivity.java
@@ -1,16 +1,22 @@
 package test.connect.geoexploreapp;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,6 +32,8 @@ import test.connect.geoexploreapp.model.ReportMarker;
 public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private boolean isCreateReportMode = false;
+    private TextView infoTextView;
 
     public MapsActivity() {
 
@@ -69,14 +77,25 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
             mapFragment.getMapAsync(this);
         }
 
-        Button btnShowReports = view.findViewById(R.id.activity_maps_report_button);
-
+        Button btnShowReports = view.findViewById(R.id.activity_maps_show_report_button);
+        Button btnCreateReport = view.findViewById(R.id.activity_maps_create_report_button);
+        infoTextView = view.findViewById(R.id.activity_maps_info_text_view);
         btnShowReports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fetchAndDisplayReports();
             }
         });
+
+        btnCreateReport.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCreateReportMode = true;
+                infoTextView.setVisibility(View.VISIBLE);
+
+            }
+        }));
+
 
         return view;
     }
@@ -85,8 +104,26 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
         LatLng ames = new LatLng(42.026224,-93.646256);
         mMap.addMarker(new MarkerOptions().position(ames).title("Test Marker"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ames,14));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if(isCreateReportMode){
+                    isCreateReportMode = false;
+                    infoTextView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
     }
+
+
 }
