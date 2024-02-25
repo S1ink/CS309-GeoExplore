99f417d49e8f995920631f7805fa7ed6ea94064c
aditin@iastate.edu <glpat-bhuszei11sThbb1j_YJs>
2024-02-25 16:42:19 -0600

    Experimenting with lambda

--- Experiment/aditin/BasicCalls/app/src/main/java/com/example/basiccalls/MainActivity.java
+++ Experiment/aditin/BasicCalls/app/src/main/java/com/example/basiccalls/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.basiccalls;
 
+import static com.example.basiccalls.api.ApiClientFactory.GetPostApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import com.example.basiccalls.api.SlimCallback;
+import com.example.basiccalls.model.Post;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,74 +28,80 @@ public class MainActivity extends AppCompatActivity {
 
         TextView apiText1 = findViewById(R.id.test);
 
-        Retrofit retroft = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+//        Retrofit retroft = new Retrofit.Builder()
+//                .baseUrl("https://jsonplaceholder.typicode.com/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
 
-        PostApi apiClient = retroft.create(PostApi.class);
+  //      PostApi apiClient = retroft.create(PostApi.class);
 
-        apiClient.getFirstPost().enqueue(new Callback<Post>(){
+//        GetPostApi().getFirstPost().enqueue(new Callback<Post>(){
+//
+//            @Override
+//            public void onResponse(Call<Post> call, Response<Post> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    Post post = response.body();
+//                    apiText1.setText(post.getBody());
+//                } else {
+//                    // Log an error message if the response is not successful
+//                    Log.e("API_CALL", "Failed to get post. Code: " + response.code());
+//                }
 
-            @Override
-            public void onResponse(Call<Post> call, Response<Post> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    Post post = response.body();
-                    apiText1.setText(post.getBody());
-                } else {
-                    // Log an error message if the response is not successful
-                    Log.e("API_CALL", "Failed to get post. Code: " + response.code());
-                }
-            }
+//
+//            @Override
+//            public void onFailure(Call<Post> call, Throwable t) {
+//
+//            }
+//        });
+        GetPostApi().getFirstPost().enqueue(new SlimCallback<Post>(response->{
+            String result = response.getId() + "\n Title: " + response.getTitle() + "\nBody: " + response.getBody();
+            apiText1.setText(result);
 
-            @Override
-            public void onFailure(Call<Post> call, Throwable t) {
+        }));
 
-            }
-        });
     }
 
 }
-
-interface PostApi{
-    @GET("posts/1")
-    Call<Post> getFirstPost();
-}
-class Post{
-    private int userId;
-    private int id;
-    private String title;
-    private String body;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-}
+//
+//interface PostApi{
+//    @GET("posts/1")
+//    Call<Post> getFirstPost();
+//}
+//class Post{
+//    private int userId;
+//    private int id;
+//    private String title;
+//    private String body;
+//
+//    public int getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(int userId) {
+//        this.userId = userId;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getBody() {
+//        return body;
+//    }
+//
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
+//}
