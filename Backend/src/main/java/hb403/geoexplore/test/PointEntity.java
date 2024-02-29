d6973deeec7d84ac23504c019c5121dbdc8cb4af
Sam Richter <samr888@iastate.edu>
2024-02-29 14:17:00 -0600

    update version and active scheme for demo 2

--- Backend/src/main/java/hb403/geoexplore/test/PointEntity.java
+++ Backend/src/main/java/hb403/geoexplore/test/PointEntity.java
@@ -6,21 +6,21 @@ import hb403.geoexplore.datatype.LocationBase;
 import jakarta.persistence.*;
 
 
-@Entity
-@Table(name = "points")
+// @Entity
+// @Table(name = "points")
 public class PointEntity extends LocationBase {
 
-	public PointEntity() {}
-	public PointEntity(Point pt) {
-		this.point = pt;
-	}
-	public PointEntity(Long id, Point pt) {
-		this.id = id;
-		this.point = pt;
-	}
+	// public PointEntity() {}
+	// public PointEntity(Point pt) {
+	// 	this.point = pt;
+	// }
+	// public PointEntity(Long id, Point pt) {
+	// 	this.id = id;
+	// 	this.point = pt;
+	// }
 
-	public Point getPoint() { return this.point; }
-	public void updatePoint(Point pt) { if(pt != null) this.point = pt; }
+	// public Point getPoint() { return this.point; }
+	// public void updatePoint(Point pt) { if(pt != null) this.point = pt; }
 
 
 }
