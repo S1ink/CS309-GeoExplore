e2ba1ec22b3d816030bd1440ed38d2039169051b
ethanamessmer <158070606+ethanamessmer@users.noreply.github.com>
2024-03-27 11:32:24 -0500

    forgot to make new branch

--- Backend/src/main/java/hb403/geoexplore/comments/controller/CommentController.java
+++ Backend/src/main/java/hb403/geoexplore/comments/controller/CommentController.java
@@ -2,13 +2,23 @@ package hb403.geoexplore.comments.controller;
 
 
 import hb403.geoexplore.comments.CommentRepo.CommentRepository;
+import hb403.geoexplore.comments.Entity.CommentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommentController {
     @Autowired
     CommentRepository commentRepository;
+    //C of Crudl
+    @PostMapping(path = "/comment/store")
+    public @ResponseBody CommentEntity commentStore (@RequestBody CommentEntity newComment){
+        commentRepository.save(newComment);
+        return newComment;
+    }
 
 
 }
