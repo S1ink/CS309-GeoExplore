04a5e518f564d816eed9761ae315fcfaea91bc0e
yusef <131935248+yusef800@users.noreply.github.com>
2024-03-29 14:33:54 -0500

    Alert dashboard now works and using read and list of alerts
    Former-commit-id: 4eb1be9091dcb9bffb30a2231b5a599b07016f55

--- Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/EmergencyDashboardFragment.java
+++ Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/EmergencyDashboardFragment.java
@@ -7,6 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import test.connect.geoexploreapp.api.AlertMarkerApi;
+import test.connect.geoexploreapp.api.ApiClientFactory;
+import test.connect.geoexploreapp.api.SlimCallback;
+import test.connect.geoexploreapp.model.AlertMarker;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +32,8 @@ public class EmergencyDashboardFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView alertInfoTextView;
 
     public EmergencyDashboardFragment() {
         // Required empty public constructor
@@ -58,7 +69,72 @@ public class EmergencyDashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency_dashboard, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_emergency_dashboard, container, false);
+        Button getAllAlertsButton = view.findViewById(R.id.getAllAlertsBtn);
+        Button getAlertByIdButton = view.findViewById(R.id.getAlertByIdBtn);
+        EditText alertIdEditText = view.findViewById(R.id.alertIdEditText);
+        alertInfoTextView = view.findViewById(R.id.alertInfoTextview);
+
+        getAllAlertsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                printAllAlerts();
+            }
+        });
+
+        getAlertByIdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String IdString = alertIdEditText.getText().toString();
+                if(!IdString.isEmpty()){
+                    try{
+                        Long id = Long.parseLong(IdString);
+                        displayAlertByID(id);
+                    } catch(NumberFormatException e){
+                        Toast.makeText(getContext(),"Enter Valid ID", Toast.LENGTH_LONG).show();
+
+                    }
+                }else {
+                    Toast.makeText(getContext(),"Enter Valid ID", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
+    }
+
+
+
+
+    private void printAllAlerts() {
+        AlertMarkerApi alertMarkerApi = ApiClientFactory.getAlertMarkerApi();
+
+        alertMarkerApi.GetAllAlertMarker().enqueue(new SlimCallback<>(alertMarkers -> {
+            StringBuilder alertInfo = new StringBuilder();
+            for (AlertMarker alertMarker : alertMarkers) {
+                alertInfo.append("ID: ").append(alertMarker.getId()).append(", Title: ").append(alertMarker.getTitle()).append(", Time: ")
+                        .append(alertMarker.getTime_created()).append("\n");
+            }
+            if (getActivity() != null) {
+                getActivity().runOnUiThread(() -> alertInfoTextView.setText(alertInfo.toString()));
+            }
+        }, "GetAllAlerts"));
+    }
+
+    private void displayAlertByID(Long id) {
+        AlertMarkerApi alertMarkerApi = ApiClientFactory.getAlertMarkerApi();
+
+        alertMarkerApi.getAlertById(id).enqueue(new SlimCallback<>(alertMarker -> {
+            if (alertMarker != null) {
+                StringBuilder alertInfo = new StringBuilder();
+                alertInfo = alertInfo.append("ID: ").append(alertMarker.getId()).append(", Title: ").append(alertMarker.getTitle()).append(", Time: ")
+                        .append(alertMarker.getTime_created()).append("\n");
+                if (getActivity() != null) {
+                    StringBuilder finalAlertInfo = alertInfo;
+                    getActivity().runOnUiThread(() -> alertInfoTextView.setText(finalAlertInfo));
+                }
+            }
+        }, "getAlertByID"));
     }
 }
