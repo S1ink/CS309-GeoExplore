dd4936b864c3891e65d51f845c1228b7e85c1006
aditin@iastate.edu <glpat-bhuszei11sThbb1j_YJs>
2024-04-11 13:16:08 -0500

    pushing with my tag
    Former-commit-id: 19a5f4ae01ff69228e03ae95f7f18faf36c9751d

--- Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/ProfileActivity.java
+++ Frontend/GeoExploreApp/app/src/main/java/test/connect/geoexploreapp/ProfileActivity.java
@@ -32,18 +32,8 @@ import test.connect.geoexploreapp.model.User;
  * create an instance of this fragment.
  */
 public class ProfileActivity extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
     private static Bundle args;
-    private String mParam2;
     private static User user;
-
 
     public ProfileActivity() {
         // Required empty public constructor
@@ -62,8 +52,8 @@ public class ProfileActivity extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
