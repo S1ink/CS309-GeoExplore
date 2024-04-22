8890cd04c7dbd1352b2ad72dd9e35324b23cbe25
aditin@iastate.edu <glpat-bhuszei11sThbb1j_YJs>
2024-04-22 06:27:04 -0500

    Added tests
    Former-commit-id: acffc6e55c19b853ff697bb9c2f3fc2371342dae

--- Frontend/GeoExploreApp/app/src/androidTest/java/test/connect/geoexploreapp/AditiSystemTest.java
+++ Frontend/GeoExploreApp/app/src/androidTest/java/test/connect/geoexploreapp/AditiSystemTest.java
@@ -1,39 +1,76 @@
 package test.connect.geoexploreapp;
 
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.core.AllOf.allOf;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 
 
-import android.support.test.rule.ActivityTestRule;
 
-// Mock the RequestServerForService class
-@RunWith(AndroidJUnit4ClassRunner.class)
-@LargeTest   // large execution time
 public class AditiSystemTest {
-
+    private boolean needLogin = false;
+    private String firstName ="A";
+    private String lastName ="N";
+    private String emailId = "AN@gmail.com";
+    private String password = "pass";
     private static final int SIMULATED_DELAY_MS = 500;
 
     @Rule   // needed to launch the activity
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<LoginSignUpActivity> activityRule = new ActivityScenarioRule<>(LoginSignUpActivity.class);
 
-    /**
-     * Start the server and run this test
-     */
+    @Before
+    public void setUp() throws Exception {
+        if (needLogin) {
+            onView(withId(R.id.login_email)).perform(typeText(emailId));
+            onView(withId(R.id.login_password)).perform(typeText(password));
+            onView(withId(R.id.login_button)).perform(click());
+
+            try {
+                Thread.sleep(SIMULATED_DELAY_MS);
+            } catch (InterruptedException e) {
+            }
+
+            // Verify that volley returned the correct value
+            onView(withId(R.id.bottomNavigationView)).check(matches(isDisplayed()));
+
+        }
+    }
+
     @Test
-    public void reverseString(){
-        String testString = "hello";
-        String resultString = "olleh";
-        // Type in testString and send request
-//        onView(withId(R.id.stringEntry))
-//                .perform(typeText(testString), closeSoftKeyboard());
-//        onView(withId(R.id.submit)).perform(click());
+    public void SignInTest(){
+        needLogin = false;
+        onView(allOf(withText("Signup"), isDescendantOfA(withId(R.id.tab_layout))))
+                .perform(click());
+
+        onView(withId(R.id.firstName)).perform(typeText(firstName));
+        onView(withId(R.id.lastName)).perform(typeText(lastName));
+        onView(withId(R.id.signup_email)).perform(typeText(emailId));
+        onView(withId(R.id.signup_password)).perform(typeText(password));
+        onView(withId(R.id.signup_confirm)).perform(typeText(password));
+        onView(withId(R.id.SignUpScrollView)).perform(swipeUp());
+        onView(withId(R.id.signup_button)).perform(click());
 
         // Put thread to sleep to allow volley to handle the request
         try {
@@ -42,31 +79,38 @@ public class AditiSystemTest {
         }
 
         // Verify that volley returned the correct value
-       // onView(withId(R.id.myTextView)).check(matches(withText(endsWith(resultString))));
+         onView(withId(R.id.bottomNavigationView)).check(matches(isDisplayed()));
 
     }
 
-    /**
-     * Start the server and run this test
-     */
     @Test
-    public void capitalizeString(){
-        String testString = "hello";
-        String resultString = "HELLO";
-        // Type in testString and send request
-//        onView(withId(R.id.stringEntry))
-//                .perform(typeText(testString), closeSoftKeyboard());
-//        onView(withId(R.id.submit2)).perform(click());
+    public void CreateObservation(){
+        needLogin = true;
+
+        onView(withId(R.id.FragmentMaps)).perform(longClick());
+        onView(withText("What do you want to create?"))
+                .inRoot(isDialog()) //
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(android.R.id.text1), withText("Observation")))
+                .inRoot(isDialog())
+                .perform(click());
+
+
+        onView(withId(R.id.editTextTitle)).perform(typeText("Lion Running"));
+        onView(withId(R.id.editTextDescription)).perform(typeText("Saw a lion running. Be careful."));
+        onView(withText("Create")).perform(click());
 
         // Put thread to sleep to allow volley to handle the request
         try {
             Thread.sleep(SIMULATED_DELAY_MS);
         } catch (InterruptedException e) {
         }
-        //onView(withId(R.id.text_simple)).check(matches(withText("Hello Espresso!")));
 
         // Verify that volley returned the correct value
-        //onView(withId(R.id.myTextView)).check(matches(withText(endsWith(resultString))));
+        onView(withId(R.id.FragmentMaps)).check(matches(isDisplayed()));
+
     }
+
 }
 
