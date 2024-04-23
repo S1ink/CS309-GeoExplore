50f92e41919307ee0c4f25e6c993462a5d542c0b
ethanamessmer <158070606+ethanamessmer@users.noreply.github.com>
2024-04-22 19:10:55 -0500

    switching to main for a sec, almost done with relationships and controller
    Former-commit-id: de65d469b4225b567e3e4b227955393aa0e62aa0

--- Backend/src/main/java/hb403/geoexplore/controllers/ImageController.java
+++ Backend/src/main/java/hb403/geoexplore/controllers/ImageController.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -26,13 +27,7 @@ public class ImageController {
     @Autowired
     private ObservationRepository observationRepository;
 
-    @Operation(summary = "gets image from repository")
-    @GetMapping(value = "/images/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    byte[] getImageById(@PathVariable long id) throws IOException {
-        Image image = imageRepository.findById(id).get();
-        File imageFile = new File(image.getFilePath());
-        return Files.readAllBytes(imageFile.toPath());
-    }
+
 
     @PostMapping("observation/image/{postId}")
     public String handleFileUpload(@RequestParam("image") MultipartFile imageFile, @PathVariable long postId)  {
@@ -50,37 +45,33 @@ public class ImageController {
             observationRepository.save(tempObs);
             imageRepository.save(image);
             return "File uploaded successfully: " + destinationFile.getAbsolutePath();
-            //System.out.println("Made it into the operation");
-            /*File destinationFile = new File(directory + File.separator + imageFile.getOriginalFilename());
-            imageFile.transferTo(destinationFile);  // save file to disk
-
-            Image image = new Image();
-            image.setFilePath(destinationFile.getAbsolutePath());
-            //ObservationMarker tempObs = observationRepository.findById(postId).get();
-            //tempObs.setImage(image);
-            //image.setObservation(tempObs);
-            //observationRepository.save(tempObs);
-            imageRepository.save(image);
-            return "File uploaded successfully: " + destinationFile.getAbsolutePath();
-            */
         } catch (Exception e) {
             e.printStackTrace();
             return "Failed to upload file: " + e.getMessage();
         }
     }
 
+    @Operation(summary = "gets image from repository using image id")
+    @GetMapping(value = "/images/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    byte[] getImageById(@PathVariable long id) throws IOException {
+        Image image = imageRepository.findById(id).get();
+        File imageFile = new File(image.getFilePath());
+        return Files.readAllBytes(imageFile.toPath());
+    }
+
+    @Operation(summary = "gets image from repository")
+
     @PutMapping("observation/image/{id}")
-    public String imageUpdate(@RequestParam("image") MultipartFile imageFile, @PathVariable long Id) {
+    public String imageUpdate(@RequestParam("image") MultipartFile imageFile, @PathVariable long id) {
         try {
             File destinationFile = new File(directory + /*File.separator +*/ Objects.requireNonNull(imageFile.getOriginalFilename()));
             System.out.println(destinationFile);
             imageFile.transferTo(destinationFile);  // save file to disk
+            Image image = imageRepository.findById(id).get();
 
-            Image image = new Image();
             image.setFilePath(destinationFile.getAbsolutePath());
-            ObservationMarker tempObs = observationRepository.findById(Id).get();
+            ObservationMarker tempObs = image.getObservation();
             tempObs.setImage(image);
-            image.setObservation(tempObs);
             observationRepository.save(tempObs);
             imageRepository.save(image);
             return "File uploaded successfully: " + destinationFile.getAbsolutePath();
@@ -89,14 +80,15 @@ public class ImageController {
             return e.toString();
         }
     }
+    @Operation(summary = "Deletes the image using the image ID")
     @DeleteMapping("observation/image/{Id}")
     public String deleteImage(@PathVariable long Id){
         try {
             Image temp = imageRepository.findById(Id).get();
-            //ObservationMarker tempObs = temp.getObservation();
-            //tempObs.setImage(null);
-            //temp.setObservation(null);
-           // observationRepository.save(tempObs);
+            ObservationMarker tempObs = temp.getObservation();
+            tempObs.setImage(null);
+            temp.setObservation(null);
+            observationRepository.save(tempObs);
             imageRepository.save(temp);
             imageRepository.deleteById(Id);
             return temp.toString();
@@ -107,6 +99,11 @@ public class ImageController {
 
     }
 
+    @Operation(summary = "List of all the image entitys but it won't actually output them as images just the object")
+    @GetMapping("images/list")
+    public @ResponseBody List<Image> listImageEntities(){
+        return imageRepository.findAll();
+    }
 
 
 }
